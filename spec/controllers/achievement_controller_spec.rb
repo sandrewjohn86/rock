bf5773a5b1e5477bafccda8fb88180ced664ff56
Stephen John <sandrewjohn86@gmail.com>
2017-01-10 00:09:32 -0400

    Test Index and Edit Action

--- spec/controllers/achievement_controller_spec.rb
+++ spec/controllers/achievement_controller_spec.rb
@@ -1,6 +1,38 @@
 require 'rails_helper'
 
 describe AchievementsController do
+
+	#index action
+	describe "GET index" do
+		it "renders :index template" do
+			get :index
+			expect(response).to render_template(:index)
+		end
+
+		it "assigns only public achievement to template" do
+			public_achievement = FactoryGirl.create(:public_achievement)
+			private_achievement = FactoryGirl.create(:private_achievement)
+			get :index
+			expect(assigns(:achievements)).to match_array([public_achievement])
+		end
+	end
+
+	#edit action
+	describe "GET edit" do
+		let(:achievement) { FactoryGirl.create(:public_achievement) }
+		
+		it "renders :edit template" do
+			get :edit, id: achievement
+			expect(response).to render_template(:edit)
+		end
+
+		it "assigns the requested achievement to template" do
+			get :edit, id: achievement
+			expect(assigns(:achievement)).to eq(achievement)
+		end
+	end
+
+	#new action
 	describe "GET new" do
 		it "renders :new template" do
 			get :new
@@ -13,6 +45,7 @@ describe AchievementsController do
 		end
 	end
 
+	#show action
 	describe "GET show" do
 		let(:achievement) {FactoryGirl.create(:public_achievement)}
 		it "renders :show template" do
@@ -26,6 +59,7 @@ describe AchievementsController do
 		end
 	end
 
+	#create action
 	describe "POST create" do
 		let(:valid_data) { FactoryGirl.attributes_for(:public_achievement) }
 		context "Valid data" do
