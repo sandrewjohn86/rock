71990be168f88c774187adc1a140b6e35ade4758
Stephen John <sandrewjohn86@gmail.com>
2017-01-19 09:39:43 -0400

    Add Test Validation

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -2,10 +2,21 @@ class Achievement < ApplicationRecord
 	belongs_to :user, optional: true
 	
 	validates :title, presence: true
+	#validates :title, uniqueness: true
+	validate :unique_title_for_one_user
 
 	enum privacy: [ :public_access, :private_access, :friends_access ]
 
 	def description_html
 		Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(description)
+	end
+
+	private
+
+	def unique_title_for_one_user
+		existing_achievement = Achievement.find_by(title: title)
+		if existing_achievement && existing_achievement.user == user
+			errors.add(:title, "you can't have two achievement with the same title")		
+		end
 	end
 end
--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Achievement, type: :model do
-  "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+  	it 'require title' do
+  		achievement = Achievement.new(title: '')
+  		#achievement.valid?
+  		#expect(achievement.errors[:title]).to include("can't be blank")
+  		#expect(achievement.errors[:title]).not_to be_empty
+  		expect(achievement.valid?).to be_falsy
+  	end
+
+  	it 'requires title to be unique for one user' do
+  		user = FactoryGirl.create(:user)
+  		first_achievement = FactoryGirl.create(:public_achievement, title: 'First Achievement', user: user)
+  		new_achievement = Achievement.new(title: 'First Achievement', user: user)
+  		expect(new_achievement.valid?).to be_falsy
+  	end
+
+  	it 'allows different users to have achievements with identical titles' do
+  		user1 = FactoryGirl.create(:user)
+  		user2 = FactoryGirl.create(:user)
+  		first_achievement = FactoryGirl.create(:public_achievement, title: 'First Achievement', user: user1)
+  		new_achievement = Achievement.new(title: 'First Achievement', user: user2)
+  		expect(new_achievement.valid?).to be_truthy
+  	end
+  end
 end
